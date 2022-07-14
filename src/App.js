e9ce52fdb199ec208a20fc128be66acf5d375153
Le Hoang Viet <vietlhse151177@fpt.edu.vn>
2022-07-14 15:35:43 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import { Component } from 'react';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import './App.css';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,11 +22,10 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
 }
-
 
 export default App;
