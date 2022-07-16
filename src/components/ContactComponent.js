2e685cd2e8384ea472280ed92638dbd6c6423bd5
Le Hoang Viet <vietlhse151177@fpt.edu.vn>
2022-07-16 13:35:44 +0700

    React Redux Forms Revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem,
         Button, Row, Col, Label } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
 const required = (val) => val && val.length;
@@ -21,6 +21,7 @@ class Contact extends Component {
     handleSubmit(values) {
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetFeedbackForm();
         // event.preventDefault();
     }
 
@@ -34,13 +35,13 @@ class Contact extends Component {
                         <BreadcrumbItem active>Contact Us</BreadcrumbItem>
                     </Breadcrumb>
                     <div className="col-12">
-                        <h3>Contact Us</h3>
+                        <h3 class="text-secondary">Contact Us</h3>
                         <hr />
                     </div>                
                 </div>
                 <div className="row row-content">
                     <div className="col-12">
-						<h3>Location Information</h3>
+						<h3 class="text-secondary">Location Information</h3>
                     </div>
                     <div className="col-12 col-sm-4 offset-sm-1">
 						<h5>Our Address</h5>
@@ -66,10 +67,11 @@ class Contact extends Component {
                 </div>
                 <div className="row row-content">
                     <div className="col-12">
-                        <h3>Send us your Feedback</h3>
+                        <h3 class="text-secondary">Send us your Feedback</h3>
                     </div>
 					<div className="col-12 col-md-9">
-                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                        {/* <LocalForm onSubmit={(values) => this.handleSubmit(values)}> */}
+                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -193,7 +195,8 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
+                        {/* </LocalForm> */}
 					</div>
                 </div>
             </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import { actions } from 'react-redux-form';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
@@ -22,7 +23,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
   
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => { dispatch(fetchDishes())}
+  fetchDishes: () => { dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 
 });
 
@@ -61,29 +63,6 @@ class Main extends Component {
       );
     };
 
-    
-    // const HomePage = () => {
-    //   return(
-    //       <Home 
-              
-    //           dish={this.props.dishes.filter((dish) => dish.featured)[0]}
-    //           promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-    //           leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-    //       />
-    //   );
-    // }
-
-    // const DishWithId = ({match}) => {
-    //   return(
-    //       // <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-    //       //   comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
-    //       <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-    //         comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-    //         addComment={this.props.addComment}
-    //   />
-    //   );
-    // };
-
     return (
       <div>
          <Header />
@@ -92,7 +71,8 @@ class Main extends Component {
             <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
             <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
             <Route path="/menu/:dishId" component={DishWithId} />
-            <Route exact path="/contactus" component={Contact} />
+            {/* <Route exact path="/contactus" component={Contact} /> */}
+            <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
             <Redirect to="/home" />
          </Switch>           
          <Footer />
